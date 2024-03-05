2a21a0577c395a93f3aef8139b4edab83d7156df
abishejk1872 <abisheknewar15@gmail.com>
2024-03-06 00:25:13 +0530

    changes

--- src/components/FullBlog.tsx
+++ src/components/FullBlog.tsx
@@ -9,9 +9,9 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
     return <div>
         <Appbar  />
         <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
+            <div className="grid  md:grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
                 <div className="col-span-8">
-                    <div className="text-5xl font-extrabold">
+                    <div className="text-2xl md:text-5xl font-extrabold">
                         {blog.title}
                     </div>
                     <div className="text-slate-500 pt-2">
@@ -21,7 +21,7 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
                         {blog.content}
                     </div>
                 </div>
-                <div className="col-span-4">
+                <div className="col-span-2 hidden md:block">
                     <div className="text-slate-600 text-lg">
                         Author
                     </div>
